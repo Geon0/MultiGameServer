b073124c9e7c1c79a7222aee4925a5bb308d8ad6
geon@ <kky8809@gmail.com>
2023-02-16 14:12:53 +0900

    서버 종료 타이밍 수정

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const app = require("express")();
 const server = require("http").createServer(app);
-// http server -> socket.io server 로 변경
-const io = require("socket.io")(server);
-
-// 접속에 사용할 포트 번호
+const SocketIO = require('socket.io');
+// 서버 연결, path는 프론트와 일치시켜준다.
+const io = SocketIO(server,{path: '/socket.io'});
 const port = 4000;
 
 //  localhost:port번호 서버에 접속하면 클라이언트로 index.html 전송
@@ -13,34 +12,68 @@ app.get("/", (req, res) => {
 
 // 소켓이 현재 연결된 방의 이름
 function getUserCurrentRoom(socket) {
-  let rooms = socket.rooms;
-
   const room1 = "Room_starwings";
   const room2 = "Room_norriGum";
   const room3 = "Room_dmc";
 
-  if (rooms.has("Room_starwings")) {
+  if (socket.rooms.has("Room_starwings")) {
     return room1;
-  } else if (rooms.has("Room_norriGum")) {
+  } else if (socket.rooms.has("Room_norriGum")) {
     return room2;
-  } else if (rooms.has("Room_dmc")) {
+  } else if (socket.rooms.has("Room_dmc")) {
     return room3;
   }
 }
 
+//namespace 지정
+// const starwings = io.of('/starwings');
+//
+// starwings.on('connection', (socket) => {
+//   console.log('starwings 네임스페이스에 접속');
+//   socket.on('disconnect', () => {
+//     console.log('starwings 네임스페이스 접속 해제');
+//   });
+// });
+
+let monsterHp = 10000;
+let count = 0;
+let roomCount = 0;
+let date_ob = new Date();
+// 몬스터 체력 받아오기
+function getMonsterHp(value) {
+  if(value) monsterHp -= value
+  return monsterHp
+}
+
 io.on("connection", (socket) => {
-  let user_name = null;
-  let weapon = 10;
-  let hp = 10000;
+  count++
+  socket.num = count
+  socket.weapon = 10;
+  console.log('클라이언트 접속',socket.id);
+  console.log('현재 시각 : ' +date_ob);
+  console.log('socket count',socket.num + ' 명 접속해 있습니다');
+
+  socket.on("disconnect", async () => {
+    console.log('클라이언트 접속 해제', socket.id);
+    clearInterval(socket.interval);
+  });
+
+  socket.on('error', (error) => {
+    console.error(error);
+  });
+
+
   socket.on("waitRoom-message", (msg) => {
     io.emit("waitRoom-message", msg);
   });
 
   socket.on("req_user_name", (msg) => {
-    user_name = msg;
+    socket.userName = msg;
   });
 
   socket.on("req_join_room", async (msg) => {
+    roomCount++
+    const value =  getMonsterHp();
     let rooms = [];
     let roomName = "Room_" + msg;
     if (!rooms.includes(roomName)) {
@@ -49,16 +82,14 @@ io.on("connection", (socket) => {
     socket.join(roomName);
     io.to(roomName).emit(
         "noti_join_room",
-        user_name + " 님이 " + roomName + "방에 입장하였습니다."
+        socket.userName + " 님이 " + roomName + "방에 입장하였습니다.",value
     );
   });
 
-  socket.on("disconnect", async () => {
-    console.log("user disconnected");
-  });
-
   socket.on("req_select_weapon", async (msg) => {
-    weapon = msg;
+    let userCurrentRoom = getUserCurrentRoom(socket);
+    socket.weapon = msg;
+    io.to(userCurrentRoom).emit("noti_weapon_change", socket.weapon,socket.userName);
   });
 
   socket.on('req_room_message', async(msg) => {
@@ -69,13 +100,16 @@ io.on("connection", (socket) => {
   socket.on("req_out_room", async (msg) => {
     let userCurrentRoom = getUserCurrentRoom(socket);
     socket.leave(userCurrentRoom);
-    io.to(userCurrentRoom).emit("noti_out_message", msg, user_name);
+    io.to(userCurrentRoom).emit("noti_out_message", msg, socket.userName);
   });
 
   socket.on("attack-damage", async () => {
     let userCurrentRoom = getUserCurrentRoom(socket);
-    hp -= weapon;
-    io.to(userCurrentRoom).emit("attack-damage", user_name, weapon, hp);
+   const value =  getMonsterHp(socket.weapon);
+   if(value < 1 ) {
+     socket.disconnect();
+   }
+    io.to(userCurrentRoom).emit("attack-damage", socket.userName, socket.weapon, value);
   });
 });
 
