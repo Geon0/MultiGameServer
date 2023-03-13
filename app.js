576e350965e074dc591e2736ad2592b8f2390c48
geon0 <kky8809@gmail.com>
2023-03-13 10:35:58 +0900

    change

--- app.js
+++ app.js
@@ -11,27 +11,17 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// 소켓이 현재 연결된 방의 이름
-function getUserCurrentRoom(socket) {
-  const room1 = "Room_starwings";
-  // const room2 = "Room_norriGum";
-  // const room3 = "Room_dmc";
+let monsterHp = 1000;
+const gameMin = 10;
 
-  // if (socket.rooms.has("Room_starwings")) {
-  //   return room1;
-  // } else if (socket.rooms.has("Room_norriGum")) {
-  //   return room2;
-  // } else if (socket.rooms.has("Room_dmc")) {
-  //   return room3;
-  // }
+// 소켓이 현재 연결된 방의 이름
+function getUserCurrentRoom() {
+  const room1 = "Room_starwings";
   return room1;
 }
 
-let monsterHp = 100;
-const gameMin = 10;
-
 function getMonsterHp(value,reset) {
-  if(reset) monsterHp = 10000;
+  if(reset) monsterHp = 1000;
   if(value) monsterHp -= value
   return monsterHp
 }
@@ -41,20 +31,10 @@ function calDmg(value) {
   const criNum = 20;
   let dmg = value;
   if(criNum > ranNum) {
-   dmg += 15
+   dmg += 50
   }
   return dmg;
 }
-
-// namespace 지정
-// const norriGum = io.of('/norriGum');
-//
-// norriGum.on('connection', (socket) => {
-//   console.log('norriGum 네임스페이스에 접속');
-//   socket.on('disconnect', () => {
-//     console.log('norriGum 네임스페이스 접속 해제');
-//   });
-// });
 
 function setEndTime(){
   let startDate = new Date();
@@ -65,22 +45,17 @@ function setEndTime(){
   return endDate;
 }
 
-
 io.on("connection", (socket) => {
-  let rooms = [];
-  socket.weapon = 10;
-
-  const value =  getMonsterHp();
-  let roomName = "Room_starwings";
-  if (!rooms.includes(roomName)) {
-    rooms.push(roomName);
-  }
+  console.log('inside connection');
+  socket.weapon = 100;
+  const mHp =  getMonsterHp();
+  console.log('mHP',mHp);
+  let roomName = getUserCurrentRoom();
   socket.join(roomName);
   io.to(roomName).emit(
       "noti_join_room",
-      socket.userName + " 님이 " + roomName + "방에 입장하였습니다.",value
+      socket.userName + " 님이 " + roomName + "방에 입장하였습니다.",mHp
   );
-
 
   let endTime = setEndTime();
   setInterval(function () {
@@ -92,8 +67,6 @@ io.on("connection", (socket) => {
       socket.disconnect();
     }
   }, 1000);
-
-
 
   socket.on("disconnect", async () => {
     console.log('클라이언트 접속 해제', socket.id);
@@ -109,33 +82,8 @@ io.on("connection", (socket) => {
     console.error(error);
   });
 
-
-  socket.on("waitRoom-message", (msg) => {
-    io.emit("waitRoom-message", msg);
-  });
-
   socket.on("req_user_name", (msg) => {
     socket.userName = msg;
-  });
-
-  // socket.on("req_join_room", async (msg) => {
-  //
-  //   const value =  getMonsterHp();
-  //   let roomName = "Room_" + msg;
-  //   if (!rooms.includes(roomName)) {
-  //     rooms.push(roomName);
-  //   }
-  //   socket.join(roomName);
-  //   io.to(roomName).emit(
-  //       "noti_join_room",
-  //       socket.userName + " 님이 " + roomName + "방에 입장하였습니다.",value
-  //   );
-  // });
-
-  socket.on("req_select_weapon", async (msg) => {
-    let userCurrentRoom = getUserCurrentRoom(socket);
-    socket.weapon = msg;
-    io.to(userCurrentRoom).emit("noti_weapon_change", socket.weapon,socket.userName);
   });
 
   socket.on('req_room_message', async(msg) => {
